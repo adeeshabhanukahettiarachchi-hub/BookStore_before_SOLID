18487e67748b3c920a47946c574d53bb6ee0cce8
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 22:49:40 +0530

    complete purchase  class

--- src/main/java/org/example/Purchase.java
+++ src/main/java/org/example/Purchase.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.util.ArrayList; import java.util.List;
 public class Purchase {
+    private final int id;
+    private final Customer customer;
+    private final List<Book> books=new ArrayList<>();
+    public Purchase(int id,Customer customer){
+        this.id=id;this.customer=customer;
+    }
+    public void addBook(Book book){
+        books.add(book);
+    }
+    public int getId(){return id;} public Customer getCustomer(){
+        return customer;
+    }
+    public List<Book> getBooks(){
+        return books;
+    }
 }
