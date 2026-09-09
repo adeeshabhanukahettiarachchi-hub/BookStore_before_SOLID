fe6ceedd3fe5cb97bcfc7ee62fb480d257d0d70c
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 22:48:08 +0530

    complete Customer class

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class Customer {
+    private final int id;
+    private final String name;
+    private final String email;
+    public Customer(int id,String name,String email){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
 }
