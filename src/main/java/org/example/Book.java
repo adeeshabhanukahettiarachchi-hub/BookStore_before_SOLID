dd4b6bd554e1cb7811bc7c86f9fbf3d187a76548
adeeshabhanukahettiarachchi-hub <adeeshabhanukahettiarachchi@gmail.com>
2026-09-09 22:44:54 +0530

    complete  book class

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,4 +1,24 @@
-package org.example;
-
+package com.bookstore;
 public class Book {
+    private final int id;
+    private final String title;
+    private final String author;
+    private final double price;
+    public Book(int id, String title, String author, double price) {
+        this.id=id;
+        this.title=title;
+        this.author=author;
+        this.price=price; }
+    public int getId(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public double getPrice(){
+        return price;
+    }
 }
